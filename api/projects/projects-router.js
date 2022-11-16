1634aed58fd73f1c2a4d770564ee9cf877edb7e8
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-16 15:44:51 -0800

    created project get by id endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,13 +4,25 @@ const Projects = require('./projects-model');
 
 const router = express();
 
-router.get('/', async (req, res, next) => {
-    const projects = await Projects.get();
+router.get('/', (req, res, next) => {
     Projects.get()
-        .then(() => {
+        .then(projects => {
             res.status(200).json(projects);
         })
         .catch(next);
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const project = await Projects.get(req.params.id);
+        if(!project) {
+            next({status: 404, message: 'project not found'})
+        } else {
+            res.status(200).json(project);
+        }
+    } catch(err) {
+        next(err);
+    }
 })
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -17,6 +17,7 @@ server.get('*', (req, res) => {
 })
 
 server.use((error, req, res, next) => { // eslint-disable-line
+    console.log(error.status);
     res.status(error.status || 500).json({message: error.message})
 })
 
